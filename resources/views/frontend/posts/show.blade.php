c869caf714ecc99d798055668d26684facbcee04
Sherman Hewitt <shewitt7@umiami.edu>
2016-05-03 13:51:46 -0400

    Added author to frontend single post

--- resources/views/frontend/posts/show.blade.php
+++ resources/views/frontend/posts/show.blade.php
@@ -7,7 +7,8 @@
 <div class="postList">
 
   <div class="post">
-    <h2>{{ $post->headline }}</h3>
+    <h2>{{ $post->headline }}</h2>
+    <h3>{{ $authors[$post->author_id] }}</h3>
     <p>{{ $post->body_text }}</p>
   </div>
 
