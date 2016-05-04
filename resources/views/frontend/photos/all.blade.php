a7e95f1531110eb1817bce4ba71d86f6f5b50465
Sherman Hewitt <shewitt7@umiami.edu>
2016-05-03 21:16:36 -0400

    Added title to top of all-photos page

--- resources/views/frontend/photos/all.blade.php
+++ resources/views/frontend/photos/all.blade.php
@@ -6,6 +6,7 @@
 @section('content')
 <div class="photoBox">
 
+  <h2 class="postList-title">Photos</h2>
   @foreach ($photos as $photo)
     <div class="photo">
       <a href="http://159.203.74.152/photo/{{$photo->id}}"><img src="{{ $photo->img_link }}" /></a>
